0d7d9a2e259ded6def08b458782b32bdcda2a867
ckaewmeechai <106985205+ckaewmeechai@users.noreply.github.com>
2023-08-21 17:57:23 +0100

    Update convertXYZ.sh

--- convertXYZ.sh
+++ convertXYZ.sh
@@ -8,20 +8,23 @@ short=short_2.pbs
 short2=short_re.pbs
 std=std_2.pbs
 wfn_file=GaO-RESTART.wfn
-XYZ_bash_dir=/work/e05/e05/ucapcka/Charge/bin/cif2xyz.sh
+XYZ_bash_dir=/work/e05/e05/ucapcka/Charge/bin
 input_dir=/work/e05/e05/ucapcka/Ov-a-Ga2O3/Ov_CN2/O_temp
+cif_file=structure.cif
 
 for i in $number ; do
     work_dir=Bond_Distortion_${i}.0%
+        /work/e05/e05/ucapcka/Charge/bin/cif2xyz.sh $work_dir/$cif_file
 
-    line=${i}
-    $XYZ_bash_dir/cif2xyz.sh $work_dir/structure.cif
-    sed -e "s/Ga3+/Ga/g" \
+    sed -i "s/Ga3+/Ga/g" \
         $work_dir/structure.xyz
-    sed -e "s/O2-/O/g" \
+    sed -i "s/O2-/O/g" \
         $work_dir/structure.xyz
     cp  $input_file $work_dir
     cp  $input_dir/$short $work_dir
     cp  $input_dir/$std $work_dir
 
+
+
 done
+
